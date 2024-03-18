127a3d9d8f922e27557c168c4064c6c3bc8c0575
namhyeop <skaguq@gmail.com>
2024-03-18 22:20:31 +0900

    docs : [step1] 더하기 테스트 코드

--- src/test/java/stringCalculator/domain/CalculatorTest.java
+++ src/test/java/stringCalculator/domain/CalculatorTest.java
@@ -1,42 +1,56 @@
 package stringCalculator.domain;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowableOfType;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+import stringCalculator.error.ErrorMessage;
 
 public class CalculatorTest {
 
     @Test
-    @DisplayName("계산기는 숫자 2개가 입력될때 더한값을 반환한다.")
+    @DisplayName("계산기는 숫자 2개가 입력될때 더한값을 반환한다")
     public void Calculator_Should_Return_Sum_When_Two_Numbers_Entered() throws Exception {
         //given
         Calculator sutCalculator = new Calculator();
 
         //when
-        long sum = sutCalculator.add(1L, 2L);
+        long sum = sutCalculator.add(1, 2);
 
         //then
-        Assertions.assertThat(sum).isEqualTo(3);
+        assertThat(sum).isEqualTo(3);
     }
 
     @Test
-    @DisplayName("계산기에 음수 2개가 전달될 때 더한 값이 정상적으로 반환되어야 한다.")
+    @DisplayName("계산기에 음수 2개가 전달될 때 더한 값이 정상적으로 반환되어야 한다")
     public void Calculator_Should_Return_Minus_When_Two_Numbers_Entered() throws Exception {
         //given
+        Calculator sutCalculator = new Calculator();
 
         //when
+        long sum = sutCalculator.add(-1, -5);
 
         //then
+        assertThat(sum).isEqualTo(-6);
     }
 
     @Test
     @DisplayName("계산기에 숫자 2개가 덧셈 되었을 때 최대 범위를 검증해야한다")
     public void Maximum_range_should_Be_Verified_When_Two_Numbers_Are_Added() throws Exception {
         //given
+        Calculator sutCalculator = new Calculator();
 
         //when
+        long sum1 = sutCalculator.add(Long.MAX_VALUE, 0);
+        long sum2 = sutCalculator.add(Long.MAX_VALUE, 1);
 
         //then
+        assertThat(sum1).isEqualTo(9223372036854775807L);
+        assertThat(sum2).isEqualTo(-9223372036854775808L);
     }
 
     @Test
