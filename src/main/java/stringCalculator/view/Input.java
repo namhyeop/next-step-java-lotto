cf441e9956c29b49f7b5898c4d84bd98316a73b1
namhyeop <skaguq@gmail.com>
2024-03-21 00:42:38 +0900

    feat : [step1] I/O 담당 클래스 구현

--- src/main/java/stringCalculator/view/Input.java
+++ src/main/java/stringCalculator/view/Input.java
@@ -4,15 +4,10 @@ import java.util.Scanner;
 
 public class Input {
 
-    private final String input;
-
     private static Scanner scanner = new Scanner(System.in);
 
-    public Input(String input) {
-        this.input = input;
-    }
-
-    public String inputNumericFormula(){
+    public static String inputNumericFormula(){
+        System.out.println("수식을 입력해주세요");
         return scanner.nextLine();
     }
 }
